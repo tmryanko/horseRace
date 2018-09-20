857a16c3afe28189d7cd5e3f1b0ac910237ac0b2
Tomer Yanko <tmryanko@gmail.com>
2018-09-20 19:58:35 +0300

    first commit

--- src/main/java/com/example/demo/Items.java
+++ src/main/java/com/example/demo/Items.java
@@ -5,47 +5,41 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity(name="ITEMS")
-public class Items {
+public class Items extends Thread{
 	
 	@Id
 	int id;
 	
-	@Column(name="NAME",nullable=false)
-	String name;
-	
-	@Column(name="AMOUNT")
-	int amount;
-	
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
-	@Column(name="INV_CODE")
-	String inventoryCode;
+
 	public Items() {
 		super();
 	}
-	public Items(int id, String name, int amount, String inventoryCode) {
+	public Items(int id) {
 		super();
 		this.id = id;
-		this.name = name;
-		this.amount = amount;
-		this.inventoryCode = inventoryCode;
+
 	}
 	public long getId() {
 		return id;
 	}
-	public String getName() {
-		return name;
-	}
-	public int getAmount() {
-		return amount;
-	}
-	public String getInventoryCode() {
-		return inventoryCode;
-	}
+
 	@Override
 	public String toString() {
-		return "items [id=" + id + ", name=" + name + ", amount=" + amount + ", inventoryCode=" + inventoryCode + "]";
+		return "items [id=" + id + "]";
+	}
+	public void run() {
+		int num=50;
+		
+		for(int i=0;i<=100;i=i+10) {
+			int num1=(int)( Math.random() * 100);
+			System.out.println("Horse number: " + id
+					 + ", percent complete: " + i );
+			try {
+				Thread.sleep(num+num1);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
 	}
 	
 }
